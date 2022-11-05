aab973007028fc8d78b329322fc6c87e292f0937
cerina Malaluan <cerinamalaluan@gmail.com>
2022-11-05 16:49:39 -0700

    back ground

--- index.js
+++ index.js
@@ -23,6 +23,24 @@ function newItem(url, left, bottom){
     })
 }
 
+function newImage(url, left, bottom){
+    for(let h = 0; h< innerHeight; h++){
+    for(let w = 0; w < innerWidth; w++){
+    newImage(url, left + w*100, bottom+h100)
+}
+    }
+}
+
+
+let horizon = window.innerHeight / 1.75
+let heightOfSky = window.innerHeight-horizon
+let heightOfGlass = horizon
+
+
+
+tile('assets/sky.png', 0, horizon, window.innerWidth/100, heightOfSky/100)
+tile('assets/grass.png', 0, 0, window.innerWidth/100, heightOfGrass/100)
+
 newImage('assets/green-character.gif', 100, 100)
 newImage('assets/tree.png', 200, 300)
 newImage('assets/pillar.png', 350, 100)
@@ -32,3 +50,4 @@ newImage('assets/well.png', 500, 425)
 newItem('assets/sword.png', 500, 405)
 newItem('assets/shield.pngassets/sheild.png', 165, 185)
 newItem('assets/staff.png', 600, 100)
+
